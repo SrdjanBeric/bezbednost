4521f927ba2a6edd2b58142dbb00a138843f7b5b
MarkoDjurdjevic <Djurdjevic.ra216.2019@uns.ac.rs>
2023-04-12 01:03:33 +0200

    kreiranje sertifikata, mali bug

--- frontend/frontend/src/app/service/certificate.service.ts
+++ frontend/frontend/src/app/service/certificate.service.ts
@@ -1,8 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CreateCertificateDto } from '../dto/Create-certificate.dto';
+import { CertificateDto } from '../dto/Certificate.dto';
 
 @Injectable({ providedIn: 'root' })
 export class CertificateService {
   constructor(private http: HttpClient) {}
+
+  private baseUrl = 'http://localhost:8082/certificate';
+
+  getMyCertificates(): Observable<CertificateDto[]> {
+    return this.http.get<CertificateDto[]>(`${this.baseUrl}/myCertificates`);
+  }
+
+  createCertificate(createCertificateDto: CreateCertificateDto):Observable<CreateCertificateDto>{
+    return this.http.post<CreateCertificateDto>(`${this.baseUrl}/create`,createCertificateDto);
+  }
+  
 }
