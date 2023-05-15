9d1c69e109c1b92bea4a24518d046719acc93b58
AnaElezovic1 <101984815+AnaElezovic1@users.noreply.github.com>
2023-05-15 18:48:54 +0200

    Dodato je create, update i rear tj get liste vestina, proveriti da li rade

--- management-app/src/main/java/managementapp/managementapp/controllers/SoftwareEngineerController.java
+++ management-app/src/main/java/managementapp/managementapp/controllers/SoftwareEngineerController.java
@@ -1,11 +1,57 @@
 package managementapp.managementapp.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import managementapp.managementapp.models.UserApp;
+import managementapp.managementapp.services.SoftwareEngineerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/softwareEngineer")
 public class SoftwareEngineerController {
 
+    @Autowired
+    public final SoftwareEngineerService softwareEngineerService;
 
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
+    @GetMapping("/allSkills")
+    @PreAuthorize("hasAnyAuthority('SOFTWARE_ENGINEER')")
+    public ResponseEntity<?> allSkills(){
+        try{
+           softwareEngineerService.skillsList();
+           return new  ResponseEntity<>(HttpStatus.OK);
+        }catch(Exception e){
+            return  new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/updateSkills")
+    @PreAuthorize("hasAnyAuthority('SOFTWARE_ENGINEER')")
+    public ResponseEntity<?>updateUser(@RequestBody List<String> updateSkills){
+        try{
+            softwareEngineerService.skillsUpdate(updateSkills);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/createSkills")
+    @PreAuthorize("hasAnyAuthority('SOFTWARE_ENGINEER')")
+    public ResponseEntity<?>createSkills(@RequestBody List<String> createSkills){
+        try{
+            softwareEngineerService.skillsCreate(createSkills);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- management-app/src/main/java/managementapp/managementapp/services/SoftwareEngineerService.java
+++ management-app/src/main/java/managementapp/managementapp/services/SoftwareEngineerService.java
@@ -1,11 +1,67 @@
 package managementapp.managementapp.services;
 
+import managementapp.managementapp.models.SoftwareEngineer;
+import managementapp.managementapp.models.UserApp;
 import managementapp.managementapp.repositories.SoftwareEngineerRepository;
+import managementapp.managementapp.repositories.UserAppRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SoftwareEngineerService {
     @Autowired
     private SoftwareEngineerRepository softwareEngineerRepository;
+
+    @Autowired
+    private UserAppRepository userAppRepository;
+
+
+    //kreirane liste vestina
+    public void skillsCreate(List<String> newSkills) {
+        UserApp loggedInUser = userAppRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        if(loggedInUser.getRole().equals("SOFTWARE_ENGINEER")){
+            SoftwareEngineer softwareEngineer = (SoftwareEngineer) loggedInUser;
+            softwareEngineer.setSkills(newSkills);
+            softwareEngineerRepository.save(softwareEngineer);
+        }else{
+            throw new RuntimeException("Can't create list of skills, because user does not Software engineer");
+        }
+
+    }
+
+    //update list of skils
+    public void skillsUpdate(List<String>updateSkills){
+        UserApp loggedInUser = userAppRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        if(loggedInUser.getRole().equals("SOFTWARE_ENGINEER")){
+            SoftwareEngineer softwareEngineer = (SoftwareEngineer) loggedInUser;
+            List<String>currentSkills = softwareEngineer.getSkills();
+            currentSkills.addAll(updateSkills);
+            softwareEngineer.setSkills(currentSkills);
+            softwareEngineerRepository.save(softwareEngineer);
+        }else{
+            throw new RuntimeException("Can't update list of skills, because user does not Software engineer");
+        }
+
+    }
+
+    //citanje celokupne liste
+    public List<String>skillsList(){
+        UserApp loggedInUser = userAppRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        SoftwareEngineer softwareEngineer = (SoftwareEngineer) loggedInUser;
+        List<String>skills = softwareEngineer.getSkills();
+        List<String>skillsList=new ArrayList<>();
+
+        for (String skill :skills
+             ) {
+            skillsList.add(skill);
+        }
+        return skillsList;
+    }
+
+
 }
